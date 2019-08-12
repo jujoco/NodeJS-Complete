e9ac076a0768b0afefa907e73b3f3f2038686a77
Julian cobos <jujocotm@gmail.com>
2019-08-12 12:17:35 -0700

    import sequelize connection

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const app = express();
+const sequelize = require('./util/database');
+const errorController = require('./controllers/error');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
+const app = express();
 const port = 3000;
 const hostname = '127.0.0.1';
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -19,8 +21,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/' });
-});
+app.use(errorController.get404);
+
+sequelize.sync()
+  .then(result => console.log(result))
+  .catch(err => console.log(err));
 
 app.listen(3000, hostname, () => console.log(`Server listening on http://${hostname}:${port}/`));
