3b1e94e167d0a7b182f517e9e85dc6ccf80c1202
Julian cobos <jujocotm@gmail.com>
2019-09-01 16:47:10 -0700

    implement sequelize connections

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const sequelize = require('./util/database');
+// controllers
 const errorController = require('./controllers/error');
+const sequelize = require('./util/database');
+
+// models
+const Product = require('./models/product');
+const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
+
+// routes
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
@@ -12,19 +24,51 @@ const port = 3000;
 const hostname = '127.0.0.1';
 
 app.set('view engine', 'ejs');
-app.set('views', 'views');
-
+app.set('views', 'views/EJS App');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync()
-  .then(result => console.log(result))
-  .catch(err => console.log(err));
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
 
-app.listen(3000, hostname, () => console.log(`Server listening on http://${hostname}:${port}/`));
+sequelize
+  // .sync({ force: true })
+  .sync()
+  .then(result => {
+    return User.findByPk(1);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'Max', email: 'test@test.com' });
+    }
+    return user;
+  })
+  .then(user => {
+    return user.createCart();
+  })
+  .then(cart => {
+    app.listen(port, hostname, () => console.log(`Server listening on http://${hostname}:${port}/`));
+  })
+  .catch(err => console.log(err));
